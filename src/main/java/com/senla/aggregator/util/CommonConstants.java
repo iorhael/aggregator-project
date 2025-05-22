196af537f9b16fc06de8caf3d88aa86a1ed413a3
Mikhail Skliar <skliarmikhail@gmail.com>
2025-05-22 22:09:51 +0300

    Fix logo loading problem

--- src/main/java/com/senla/aggregator/util/CommonConstants.java
+++ src/main/java/com/senla/aggregator/util/CommonConstants.java
@@ -2,6 +2,7 @@ package com.senla.aggregator.util;
 
 public final class CommonConstants {
     public static final String COMMA = ",";
+    public static final String PNG_CONTENT_TYPE = "image/png";
     public static final String TMP_FILE_EXTENSION = ".tmp";
     public static final String JSON_FILE_EXTENSION = ".json";
 
